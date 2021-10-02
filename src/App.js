3df51ed6bb3fcd3a1cba59022517606316bc578b
musasazib <musasazib@gmail.com>
2021-10-02 17:57:53 +0600

    multiple value in context

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [house, setHouse] = useState(5);
   const ornaments = "Diamond Ring"
   return (
-    <RingContext.Provider value={ornaments}>
+    <RingContext.Provider value={[ornaments, house]}>
       <div className="App">
         <button onClick={() => setHouse(house + 1)}>Buy a new House</button>
         <GrandFather house={house} ornaments={ornaments}></GrandFather>
--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../../App';
 
-const Aunty = (props) => {
-    const { house } = props;
+const Aunty = () => {
+    const [ornaments, house] = useContext(RingContext)
     return (
         <div>
             <h2>Aunty</h2>
             <p>House: {house}</p>
+            <small>{ornaments}</small>
         </div>
     );
 };
